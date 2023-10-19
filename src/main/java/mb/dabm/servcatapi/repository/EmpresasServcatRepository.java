ea5aef3742097db82c37564196d02185e6501bf9
Daniel <dansilnasc@gmail.com>
2023-10-18 23:27:57 -0300

    [TI] Implementados recursos da Tabela SUPPLIER e REFERENCE_NUMBER

--- src/main/java/mb/dabm/servcatapi/repository/EmpresasServcatRepository.java
+++ src/main/java/mb/dabm/servcatapi/repository/EmpresasServcatRepository.java
@@ -38,5 +38,73 @@ public interface EmpresasServcatRepository
     Page<EmpresasServcat> getByAll(Pageable pageable);
 
 
+    @Query(value = "SELECT S.CAGE_CODE\n" +
+        "      ,S.CAGE_STATUS\n" +
+        "      ,S.CAGE_TYPE\n" +
+        "      ,S.CAGE_CAO\n" +
+        "      ,S.CAGE_ADP\n" +
+        "      ,S.CAGE_RPLM\n" +
+        "      ,S.CAGE_ASSOC\n" +
+        "      ,S.CAGE_AFFIL\n" +
+        "      ,S.CAGE_SIZE\n" +
+        "      ,S.CAGE_PRIMARY_BUSINESS\n" +
+        "      ,S.CAGE_TYPE_OF_BUSINESS\n" +
+        "      ,S.CAGE_WOMAN_OWNED\n" +
+        "      ,S.CAGE_SIC_CODES\n" +
+        "      ,S.CAGE_FAX\n" +
+        "      ,S.CAGE_COMPANY_NAME\n" +
+        "      ,S.CAGE_COMPANY_ADDRESS\n" +
+        "      ,S.CAGE_PO_BOX\n" +
+        "      ,S.CAGE_CITY\n" +
+        "      ,S.CAGE_STATE\n" +
+        "      ,S.CAGE_COUNTRY\n" +
+        "      ,S.CAGE_ZIP_CODE\n" +
+        "      ,S.CAGE_TELEPHONE\n" +
+        "      ,S.CAGE_FORMER_ADDRESS\n" +
+        "FROM SUPPLIER S\n" +
+        "WHERE CAGE_CODE = :cageCode",
+    nativeQuery = true)
+    EmpresasServcat getByCageCodeId (String cageCode);
+
+
+
+    @Query(value = "SELECT S.CAGE_CODE\n" +
+        "      ,S.CAGE_STATUS\n" +
+        "      ,S.CAGE_TYPE\n" +
+        "      ,S.CAGE_CAO\n" +
+        "      ,S.CAGE_ADP\n" +
+        "      ,S.CAGE_RPLM\n" +
+        "      ,S.CAGE_ASSOC\n" +
+        "      ,S.CAGE_AFFIL\n" +
+        "      ,S.CAGE_SIZE\n" +
+        "      ,S.CAGE_PRIMARY_BUSINESS\n" +
+        "      ,S.CAGE_TYPE_OF_BUSINESS\n" +
+        "      ,S.CAGE_WOMAN_OWNED\n" +
+        "      ,S.CAGE_SIC_CODES\n" +
+        "      ,S.CAGE_FAX\n" +
+        "      ,S.CAGE_COMPANY_NAME\n" +
+        "      ,S.CAGE_COMPANY_ADDRESS\n" +
+        "      ,S.CAGE_PO_BOX\n" +
+        "      ,S.CAGE_CITY\n" +
+        "      ,S.CAGE_STATE\n" +
+        "      ,S.CAGE_COUNTRY\n" +
+        "      ,S.CAGE_ZIP_CODE\n" +
+        "      ,S.CAGE_TELEPHONE\n" +
+        "      ,S.CAGE_FORMER_ADDRESS\n" +
+        "FROM SUPPLIER S\n" +
+        "WHERE CAGE_COUNTRY = :cageCountry",
+        nativeQuery = true)
+    Page<EmpresasServcat> getByCageCountry (String cageCountry, Pageable pageable);
+
+
+
+@Query(value = """
+    SELECT *
+    FROM SUPPLIER
+    WHERE 1 = 1
+    AND CAGE_COMPANY_NAME LIKE :cageCompanyName
+    """,
+    nativeQuery = true)
+    Page<EmpresasServcat> getByCageCompanyName (String cageCompanyName, Pageable pageable);
 
 }
--- src/main/java/mb/dabm/servcatapi/service/EmpresasServcatService.java
+++ src/main/java/mb/dabm/servcatapi/service/EmpresasServcatService.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import mb.dabm.servcatapi.entity.EmpresasServcat;
 import mb.dabm.servcatapi.repository.EmpresasServcatRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,5 +21,22 @@ public class EmpresasServcatService {
 
     public Page<EmpresasServcat> findByAll(int page, int size) { return repository.getByAll(PageRequest.of(page, size));}
 
+    public EmpresasServcat findByCageCodeId (String cageCode) {
+        return repository.getByCageCodeId(cageCode);}
+
+    public Page<EmpresasServcat> findByCageCountry(String cageCountry, int page, int size) {
+        return repository.getByCageCountry(cageCountry, PageRequest.of(page, size));
+    }
+
+    public Page<EmpresasServcat> getByCageCompanyName(String cageCompanyName, int page, int size) {
+        long count = cageCompanyName.chars().filter(ch -> ch == '*').count();
+
+        if (count > 0) {
+            cageCompanyName = cageCompanyName.replace("*", "%");
+        } else {
+            cageCompanyName = "%" + cageCompanyName + "%";
+        }
+        return repository.getByCageCompanyName(cageCompanyName, PageRequest.of(page, size));
+    }
 
 }
